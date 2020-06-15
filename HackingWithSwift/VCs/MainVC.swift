1160ac458f5cb8be6e6980db92c94b08495a3192
Ivan Terziev <ivanterziev38@gmail.com>
2020-06-15 21:06:44 +0300

    Initial Commit

---
+++ HackingWithSwift/VCs/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackingWithSwift
+//
+//  Created by Ivan Terziev on 15.06.20.
+//  Copyright © 2020 Ivan Terziev. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - App lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+    
+    // MARK: - Methods
+}
---
+++ HackingWithSwift/VCs/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HackingWithSwift
+//
+//  Created by Ivan Terziev on 15.06.20.
+//  Copyright © 2020 Ivan Terziev. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+}
